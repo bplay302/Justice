3c4908b9150f98afe87eac0b7256fc31b11e0941
bplay302 <haverbarak@gmail.com>
2025-02-01 13:31:32 +0200

    window opened, and ball drown

--- Justice/main.c
+++ Justice/main.c
@@ -1,8 +1,34 @@
 #include <raylib.h>
 
+#define SCREEN_LEN 600
+
+typedef struct Ball_t
+{
+    Vector2 pos;
+    int radius;
+}Ball;
+
 int main(void)
 {
-	TraceLog(LOG_INFO, "hello world");
+    Ball ball = { {SCREEN_LEN / 2, SCREEN_LEN / 2}, 10 };
+    InitWindow(SCREEN_LEN, SCREEN_LEN, "Justice");
 
-	return 0;
+    SetTargetFPS(60);
+
+    // Main game loop
+    while (!WindowShouldClose())    // Detect window close button or ESC key
+    {
+        BeginDrawing();
+
+        ClearBackground(BLACK);
+
+        DrawCircle(ball.pos.x, ball.pos.y, ball.radius, GREEN);
+        DrawRectangle(ball.pos.x - ball.radius * 3, ball.pos.y - ball.radius, 10, 20, DARKGREEN);
+
+        EndDrawing();
+    }
+
+    CloseWindow();        // Close window and OpenGL context
+
+    return 0;
 }
